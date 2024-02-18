a757a429aeb7f0f123e043a4c271aaeaa5b2704b
ramyasrikontham <ramyasrikontham@gmail.com>
2024-02-18 15:01:26 -0600

    Initial Commit

---
+++ SwiftUIClass1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIClass1
+//
+//  Created by Ramyasri Kontham on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIClass1/SwiftUIClass1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIClass1App.swift
+//  SwiftUIClass1
+//
+//  Created by Ramyasri Kontham on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIClass1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
